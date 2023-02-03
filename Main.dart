4e270d0e55e15299c4fcc93ea5527b6acba80476
dmar1313 <122155033+dmar1313@users.noreply.github.com>
2023-02-02 19:27:05 -0600

    Create Main.dart

---
+++ Main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gpt3_chatbot/models/message.dart';
+import 'package:gpt3_chatbot/screens/chat_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GPT-3 Chatbot',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
